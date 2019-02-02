03d4279ddb0b5f14a6dd67056fc1facf9dd41db7
rasor <soren@rasor.dk>
2019-02-02 18:03:16 +0100

    passed ensure

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -7,22 +7,29 @@ import { Component } from '@stencil/core';
 })
 export class AppHome {
 
-  getString(){
-    return 'hallo world!'
+  ensure(value){
+    if (value === undefined) throw 'undef';
+    return value;
   }
+
   render() {
     return (
       <div class='app-home'>
-        <p>Solving puzzle 
-          <a href='https://www.testdome.com/questions/21948?generatorId=20&questionIds=21948%2C13651%2C13919%2C7282&type=FromTest'>Ensure</a>
-        </p>
-        <p>{this.getString()}</p>
+        <div>
+          <p>Solving puzzle 
+            <a href='https://www.testdome.com/questions/21948?generatorId=20&questionIds=21948%2C13651%2C13919%2C7282&type=FromTest'>Ensure</a>
+          </p>
+          <p>{this.ensure(undefined)}</p>
+          <p>{this.ensure('sss')}</p>
+        </div>
 
-        {/* <stencil-route-link url='/profile/stencil'>
-          <button>
-            Profile page
-          </button>
-        </stencil-route-link> */}
+        <div>
+          {/* <stencil-route-link url='/profile/stencil'>
+            <button>
+              Profile page
+            </button>
+          </stencil-route-link> */}
+        </div>
       </div>
     );
   }
